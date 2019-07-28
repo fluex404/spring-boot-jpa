054c3d410738ed73802bbd9a342d32b39502b304
Isa <isaabqari@gmail.com>
2019-07-28 14:24:23 +0700

    revisi

--- spring-boot-jpa/src/main/java/com/fluex404/springbootjpa/exception/ResourceNotFoundException.java
+++ spring-boot-jpa/src/main/java/com/fluex404/springbootjpa/exception/ResourceNotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
+	
 	public ResourceNotFoundException() {
 		super();
 	}
